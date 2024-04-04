151edb0106de5a8eb21491ec0f042296c592c5b4
= <=>
2024-04-05 01:26:32 +0530

    update code

--- dist/controller/handlers/upiHandler.js
+++ dist/controller/handlers/upiHandler.js
@@ -8,22 +8,46 @@ Object.defineProperty(exports, "upiRouteOptions", {
         return upiRouteOptions;
     }
 });
-const _Transaction = require("../../models/Transaction");
+const _UpiTransaction = require("../../models/UpiTransaction");
+const _UpiTransactionReal = require("../../models/UpiTransactionReal");
+const _regexMatcher = require("../../utils/regexMatcher");
 const _upiRouteSchema = require("../schemas/upiRouteSchema");
 async function upiHandler(request, reply) {
     try {
-        const body = request.body;
-        // const transaction = await Transaction.findOne({
-        //   title: body.title,
+        // const body = request.body as UpiRouteRequestBody;
+        // // const transaction = await Transaction.findOne({
+        // //   title: body.title,
+        // // });
+        // // if (transaction) {
+        // //   throw new Error("Transaction Already Inserted!");
+        // // }
+        // const newTransaction = new Transaction(body);
+        // await newTransaction.save();
+        // reply.send({
+        //   message: "Transaction Saved",
+        //   data: newTransaction,
         // });
-        // if (transaction) {
-        //   throw new Error("Transaction Already Inserted!");
-        // }
-        const newTransaction = new _Transaction.Transaction(body);
-        await newTransaction.save();
+        const body = request.body;
+        const uniqId = await (0, _regexMatcher.regexMatcher)(body.text);
+        // Find the existing transaction
+        const existingTransaction = await _UpiTransaction.UpiTransaction.findOne({
+            uniqId
+        });
+        // If a transaction with the provided unique ID doesn't exist, return an error response
+        if (!existingTransaction) {
+            throw new Error("Transaction with the provided unique ID does not exist.");
+        }
+        // Update the status of the existing transaction to "success"
+        existingTransaction.status = "success";
+        await existingTransaction.save();
+        // Create a new real transaction object based on the existing transaction
+        const newRealTransaction = new _UpiTransactionReal.UpiTransactionReal(existingTransaction);
+        await newRealTransaction.save();
+        // Delete the existing transaction if needed
+        await existingTransaction.deleteOne();
+        // Send the response with the new real transaction
         reply.send({
-            message: "Transaction Saved",
-            data: newTransaction
+            transaction: newRealTransaction
         });
     } catch (error) {
         throw error;
--- src/controller/handlers/upiHandler.ts
+++ src/controller/handlers/upiHandler.ts
@@ -1,26 +1,56 @@
 import { FastifyReply, FastifyRequest, RouteShorthandOptionsWithHandler } from "fastify";
-import { Transaction } from "../../models/Transaction";
-import { UpiRouteRequestBody, upiRouteSchema } from "../schemas/upiRouteSchema";
+import { UpiTransaction } from "../../models/UpiTransaction";
+import { UpiTransactionReal } from "../../models/UpiTransactionReal";
+import { regexMatcher } from "../../utils/regexMatcher";
+import { UpdateUpiRequestBody } from "../schemas/updateUpiTransaction";
+import { upiRouteSchema } from "../schemas/upiRouteSchema";
 
 async function upiHandler(request: FastifyRequest, reply: FastifyReply) {
   try {
-    const body = request.body as UpiRouteRequestBody;
+    // const body = request.body as UpiRouteRequestBody;
 
-    // const transaction = await Transaction.findOne({
-    //   title: body.title,
+    // // const transaction = await Transaction.findOne({
+    // //   title: body.title,
+    // // });
+
+    // // if (transaction) {
+    // //   throw new Error("Transaction Already Inserted!");
+    // // }
+
+    // const newTransaction = new Transaction(body);
+
+    // await newTransaction.save();
+
+    // reply.send({
+    //   message: "Transaction Saved",
+    //   data: newTransaction,
     // });
 
-    // if (transaction) {
-    //   throw new Error("Transaction Already Inserted!");
-    // }
+    const body = request.body as UpdateUpiRequestBody;
+    const uniqId = await regexMatcher(body.text);
 
-    const newTransaction = new Transaction(body);
+    // Find the existing transaction
+    const existingTransaction = await UpiTransaction.findOne({ uniqId });
 
-    await newTransaction.save();
+    // If a transaction with the provided unique ID doesn't exist, return an error response
+    if (!existingTransaction) {
+      throw new Error("Transaction with the provided unique ID does not exist.");
+    }
 
+    // Update the status of the existing transaction to "success"
+    existingTransaction.status = "success";
+    await existingTransaction.save();
+
+    // Create a new real transaction object based on the existing transaction
+    const newRealTransaction = new UpiTransactionReal(existingTransaction);
+    await newRealTransaction.save();
+
+    // Delete the existing transaction if needed
+    await existingTransaction.deleteOne();
+
+    // Send the response with the new real transaction
     reply.send({
-      message: "Transaction Saved",
-      data: newTransaction,
+      transaction: newRealTransaction,
     });
   } catch (error) {
     throw error;
